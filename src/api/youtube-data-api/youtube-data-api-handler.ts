50ca3fbaf1bce8a21e16353d20b084263e7e8eff
Nishant Arora <nish54@gmail.com>
2020-11-27 03:04:59 -0600

    youtube api connection + data received

---
+++ src/api/youtube-data-api/youtube-data-api-handler.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { YOUTUBE_API_KEY } from '../../config/config';
+import { logger } from '../../utilities/logger';
+import { YoutubeResponse } from './youtube-data-api-response';
+
+const endpoint = 'https://www.googleapis.com/youtube/v3';
+const videoBaseUrl = 'https://www.youtube.com/watch?v=';
+
+const requestClient = axios.create({
+  baseURL: endpoint,
+  params: {
+    key: YOUTUBE_API_KEY,
+  },
+});
+
+export const search = async (query: string): Promise<YoutubeResponse> => {
+  try {
+    const { data: body } = await requestClient.get('/search', {
+      params: { q: query },
+    });
+    logger.info('YouTube API Search', `query: ${query}`, `response: ${body}`);
+    return body;
+  } catch (e) {
+    logger.info('YouTube API Search Error!!!', `${e.message}`);
+    throw new Error('Youtube Search Error');
+  }
+};
+
+export const getPlayableUrl = (videoId: string): string =>
+  `${videoBaseUrl}${videoId}`;
---
+++ src/api/youtube-data-api/youtube-data-api-response.ts
@@ -0,0 +1,14 @@
+export interface YoutubeResponseSearchItem {
+  id: {
+    videoId: string;
+  };
+}
+
+export interface YoutubeResponse {
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+
+  items: YoutubeResponseSearchItem[];
+}
